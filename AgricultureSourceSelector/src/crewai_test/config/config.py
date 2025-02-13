1389aa59c5d38b7094cba7e55fa07cec0cf08100
Tarık Gülezgin <81231546+gulezgin@users.noreply.github.com>
2025-02-13 21:59:11 +0300

    Update config.py

--- AgricultureSourceSelector/src/crewai_test/config/config.py
+++ AgricultureSourceSelector/src/crewai_test/config/config.py
@@ -6,17 +6,17 @@ class OpenAIConfig:
     endpoint: str
     deployment: str
     subscription_key: str
-    api_version: str = "2024-05-01-preview"
-    location: str = "francecentral"
+    api_version: str = 
+    location: str = "
 
 @dataclass
 class RulesConfig:
     rules: Dict[str, Any]
 
 openai_config = OpenAIConfig(
-    endpoint="https://arconarswedendev.openai.azure.com/",
-    deployment="gpt-4o-1",
-    subscription_key="e7876b13cf654bbeabd5892d50b6d5d5",
-    api_version="2024-02-15-preview",
-    location="swedencentral"
+    endpoint=
+    deployment=
+    subscription_key=
+    api_version=
+    location=
 )
